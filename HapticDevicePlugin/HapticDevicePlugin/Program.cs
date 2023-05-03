c5b6c4adcc7ed7467604c58744d80ee8999eab04
DW <weng840623@gmail.com>
2023-05-03 21:13:18 +0800

    add framework components

---
+++ HapticDevicePlugin/HapticDevicePlugin/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using RedisEndpoint;
+
+namespace HapticDevicePlugin_HapticVest {
+    class Program {
+        public static Subscriber symmetrical_event_subscriber = new Subscriber("localhost", 6379);
+        public static Subscriber nonsymmetrical_event_subscriber = new Subscriber("localhost", 6379);
+
+        public static HapticVestPlugin hapticVestPlugin = new HapticVestPlugin();
+        public static string symmetrical_event_channel;
+        public static string nonsymmetrical_event_channel;
+        static void Main(string[] args) {
+            Console.WriteLine("HapticDevicePlugin start");
+            symmetrical_event_channel = args[0];  
+            nonsymmetrical_event_channel = args[1];
+            symmetrical_event_subscriber.SubscribeTo(symmetrical_event_channel);
+            nonsymmetrical_event_subscriber.SubscribeTo(nonsymmetrical_event_channel);
+            symmetrical_event_subscriber.msgQueue.OnMessage(msg =>hapticVestPlugin.msg_handler("sym",msg.Message));
+            nonsymmetrical_event_subscriber.msgQueue.OnMessage(msg => hapticVestPlugin.msg_handler("nonsym", msg.Message));
+            while (true) {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                switch (key.Key) {
+                    case ConsoleKey.Enter:
+                        break;
+                    default:
+                        continue;
+                }
+
+            }
+        }
+    }
+    class HapticVestPlugin {
+
+        private LinkedList<HapticEvent> eventList = new LinkedList<HapticEvent>();
+        public HapticVestPlugin() {
+
+        }
+        public void msg_handler(string type, string msg) {
+            // 解析msg
+            string[] haptic_event_info_lists = msg.Split('|');
+            string[] state_info_list = haptic_event_info_lists[4].Split(' ');
+            // 做成HapticEvent
+            HapticEvent temp = new HapticEvent(
+                EventTime: haptic_event_info_lists[0],
+                SourceTypeName: haptic_event_info_lists[1],
+                EventType: haptic_event_info_lists[2],
+                EventName: haptic_event_info_lists[3],
+                Amp: state_info_list[1],
+                Freq: state_info_list[3],
+                Dur: state_info_list[5],
+                EnListTime: DateTime.Now
+            );
+            // add into eventList
+            lock (eventList) {
+                eventList.AddLast(temp);
+            }
+        }
+        public void plugin_thread() {
+
+        }
+
+    }
+
+    class HapticEvent {
+        // 性質
+        public float Duration; // 這三個基本資訊合起來是 channel 裡的 stateinfo 
+        public float Amplitude;
+        public float Frequency;
+        public DateTime EventDayTime;// new CultureInfo("en-US");
+
+        public string EventType;
+        public string EventTime;
+        public string EventName;
+        public string SourceTypeName;
+
+        public DateTime EnListTime;
+
+
+        public HapticEvent(string EventTime, string SourceTypeName, string EventType, string EventName, string Amp, string Freq, string Dur, DateTime EnListTime) {
+            this.Duration = Convert.ToSingle(Dur);
+            this.Amplitude = Convert.ToSingle(Amp);
+            this.Frequency = Convert.ToSingle(Freq);
+
+            this.EventDayTime = DateTime.Parse(EventTime, new CultureInfo("en-US"), DateTimeStyles.NoCurrentDateDefault);
+            this.EventTime = EventTime;
+            this.EventType = EventType;
+            this.EventName = EventName;
+            this.SourceTypeName = SourceTypeName;
+
+            this.EnListTime = EnListTime;
+
+        }
+    };
+}
---
+++ VibrationSignalClassifier/VibrationSignalClassifier/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RedisEndpoint;
+
+namespace vibration_signal_classifier {
+
+
+    class Program {
+
+        private static Subscriber VibrationSignalSubscriber = new Subscriber("localhost", 6379);
+        private static string VibrationSignalChannelName;
+        private static Thread subThread = new Thread(SubscribeThread);
+
+        private static VibrationSignalClassifier vibrationSignalClassifier = new VibrationSignalClassifier();
+        static void Main(string[] args) {
+            VibrationSignalChannelName = args[0];
+            subThread.Start();
+            Console.WriteLine("normal start");
+            Console.WriteLine("press enter to stop");
+            while (true) {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                switch (key.Key) {
+                    case ConsoleKey.Enter:
+                        subThread.Abort();
+                        return;
+                    default:
+                        break;
+                }
+            }
+        }
+        static void SubscribeThread() {
+            Console.WriteLine("start sub " + VibrationSignalChannelName);
+            VibrationSignalSubscriber.SubscribeTo(VibrationSignalChannelName);
+            VibrationSignalSubscriber.msgQueue.OnMessage(msg => vibrationSignalClassifier.msg_Handler(msg.Message));
+        }
+
+    }
+
+    class VibrationSignalClassifier {
+        private readonly static int k_DELAY_TIME_WINDOW = 80; // Delay time to detect symmetric signals
+        private LinkedList<HapticEvent> eventList = new LinkedList<HapticEvent>();
+        private Publisher nonsymmetrical_event_publisher = new Publisher("localhost", 6379);
+        private Publisher symmetrical_event_publisher = new Publisher("localhost", 6379);
+        private bool IsSymmetric() {
+            // 檢查第一個有沒有跟人對稱
+            lock (eventList) {
+                var iterate_node = eventList.First;
+                var nowEvent = eventList.First.Value;
+                while (iterate_node.Next != null) {
+                    var next_node = iterate_node.Next;
+                    var nextEvent = next_node.Value;
+                    if (nextEvent.get_duration == nowEvent.get_duration
+                        && nextEvent.get_amplitude == nowEvent.get_amplitude
+                        && nextEvent.get_freqeuncy == nowEvent.get_freqeuncy
+                        && nextEvent.get_source_type_name != nowEvent.get_source_type_name) {
+                        // 拔掉對稱
+                        eventList.Remove(next_node);
+                        return true;
+
+                    }
+                    iterate_node = next_node;
+                }
+            }
+
+            return false;
+        }
+        public void msg_Handler(string msg) {
+            // 解析msg
+            string[] haptic_event_info_lists = msg.Split('|');
+            string[] state_info_list = haptic_event_info_lists[4].Split(' ');
+            // 做成HapticEvent
+            HapticEvent temp = new HapticEvent(
+                EventTime: haptic_event_info_lists[0],
+                SourceTypeName: haptic_event_info_lists[1],
+                EventType: haptic_event_info_lists[2],
+                EventName: haptic_event_info_lists[3],
+                Amp: state_info_list[1],
+                Freq: state_info_list[3],
+                Dur: state_info_list[5],
+                EnListTime: DateTime.Now
+            );
+            // add into eventList
+            lock (eventList) {
+                eventList.AddLast(temp);
+            }
+
+        }
+
+        public void classifier_thread() {
+
+            HapticEvent nowEvent; // 後面會一直用到 在這邊宣告應該比較不浪費宣告又release的時間
+            while (true) {
+                // 看list裡面的時間過期沒
+                while (eventList.Count() != 0) {
+                    DateTime nowTime = DateTime.Now;
+
+                    lock (eventList) {
+                        nowEvent = eventList.First();
+                    }
+
+                    // 給個time window XX ms 看這個區間有沒有產生對稱震動 這個區間太大的話會很誤觸到雙手對稱
+                    if ((nowTime - nowEvent.get_enter_list_daytime).TotalMilliseconds < k_DELAY_TIME_WINDOW)
+                        break;
+
+                    if (IsSymmetric()) {
+                        Console.WriteLine($"SYM  {nowEvent.get_event_daytime.ToString("MM/dd HH:mm:ss.fff", new CultureInfo("en-US")) + "  :"}|{nowEvent.get_source_type_name}|{nowEvent.get_amplitude}|{nowEvent.get_freqeuncy}|{nowEvent.get_duration}|{nowEvent.get_event_name}");
+                        // sym_pattern_1(ref nowEvent);
+                        // publish symmetrical event
+                        symmetrical_event_publisher.Publish("nonsymmetrical_event", nowEvent.ToString()); // 要publish什麼是隨便填的 之後再補
+                    }
+                    else {
+                        Console.WriteLine($"NONE {nowEvent.get_event_daytime.ToString("MM/dd HH:mm:ss.fff", new CultureInfo("en-US")) + "  :"}|{nowEvent.get_source_type_name}|{nowEvent.get_amplitude}|{nowEvent.get_freqeuncy}|{nowEvent.get_duration}|{nowEvent.get_event_name}");
+                        // nonsym_pattern_1(ref nowEvent);
+                        // publish nonsymmetrical event
+                        nonsymmetrical_event_publisher.Publish("symmetrical_event", nowEvent.ToString()); // 要publish什麼是隨便填的 之後再補
+                    }
+                    
+                    lock (eventList) {
+                        eventList.RemoveFirst();
+                    }
+                }
+            }
+
+        }
+    }
+
+    class HapticEvent {
+        private readonly float duration_; // 這三個基本資訊合起來是 channel 裡的 stateinfo 
+        private readonly float amplitude_;
+        private readonly float frequency_;
+        private readonly DateTime event_daytime_;// new CultureInfo("en-US");
+
+        private readonly string event_type_;
+        private readonly string event_time_;
+        private readonly string event_name_;
+        private readonly string source_type_name_;
+
+        private readonly DateTime enter_list_time_;
+
+
+        public HapticEvent(string EventTime, string SourceTypeName, string EventType, string EventName, string Amp, string Freq, string Dur, DateTime EnListTime) {
+            this.duration_ = Convert.ToSingle(Dur);
+            this.amplitude_ = Convert.ToSingle(Amp);
+            this.frequency_ = Convert.ToSingle(Freq);
+            this.event_daytime_ = DateTime.Parse(EventTime, new CultureInfo("en-US"), DateTimeStyles.NoCurrentDateDefault);
+
+            this.event_time_ = EventTime;
+            this.event_type_ = EventType;
+            this.event_name_ = EventName;
+            this.source_type_name_ = SourceTypeName;
+
+            this.enter_list_time_ = EnListTime;
+
+        }
+        public float get_duration { get => duration_; }
+        public float get_amplitude { get => amplitude_; }
+        public float get_freqeuncy { get => frequency_; }
+        public DateTime get_event_daytime { get => event_daytime_; }
+        public string get_event_type { get => event_type_; }
+        public string get_event_time { get => event_time_; }
+        public string get_event_name { get => event_name_; }
+        public string get_source_type_name { get => source_type_name_; }
+        public DateTime get_enter_list_daytime { get => enter_list_time_; }
+    }
+}
