4aa5310928b6a7a18c02f1bc60d7e9aa3676b063
SED2023S_group1 <sedgroup1@ntu.edu.tw>
2023-05-12 11:27:56 +0800

    update components

--- HapticDevicePlugin/HapticDevicePlugin/Program.cs
+++ HapticDevicePlugin/HapticDevicePlugin/Program.cs
@@ -5,6 +5,8 @@ using bHapticsLib;
 using RedisEndpoint;
 using HapticEventNS;
 using System.IO;
+using System.Threading;
+using System.Linq;
 
 namespace HapticDevicePlugin_HapticVest {
     class Program {
@@ -16,39 +18,64 @@ namespace HapticDevicePlugin_HapticVest {
         public static string nonsymmetrical_event_channel;
         static void Main(string[] args) {
             Console.WriteLine("HapticDevicePlugin start");
-            symmetrical_event_channel = args[0];  
-            nonsymmetrical_event_channel = args[1];
+            //symmetrical_event_channel = args[0];  
+            symmetrical_event_channel = "symmetrical_event";
+            //nonsymmetrical_event_channel = args[1];
+            nonsymmetrical_event_channel = "nonsymmetrical_event";
             symmetrical_event_subscriber.SubscribeTo(symmetrical_event_channel);
             nonsymmetrical_event_subscriber.SubscribeTo(nonsymmetrical_event_channel);
-            symmetrical_event_subscriber.msgQueue.OnMessage(msg =>hapticVestPlugin.msg_handler("sym",msg.Message));
-            nonsymmetrical_event_subscriber.msgQueue.OnMessage(msg => hapticVestPlugin.msg_handler("nonsym", msg.Message));
+            symmetrical_event_subscriber.msgQueue.OnMessage(msg =>hapticVestPlugin.sym_msg_handler("sym",msg.Message));
+            nonsymmetrical_event_subscriber.msgQueue.OnMessage(msg => hapticVestPlugin.non_sym_msg_handler("nonsym", msg.Message));
+
+            Thread plugin_thread = new Thread(hapticVestPlugin.plugin_thread);
+            plugin_thread.Start();
             while (true) {
                 ConsoleKeyInfo key = Console.ReadKey(true);
                 switch (key.Key) {
                     case ConsoleKey.Enter:
+                        plugin_thread.Abort();
                         break;
                     default:
                         continue;
                 }
-
             }
         }
     }
     class HapticVestPlugin {
         // bHapticLib連線
         private static bHapticsConnection Connection;
-        string assembly_location = Path.GetDirectoryName(typeof(VestController).Assembly.Location);
+        string assembly_location = Path.GetDirectoryName(typeof(VestObject).Assembly.Location);
 
         // bHapticObject
         public static VestObject VestLeft;
+        public static VestObject VestRight;
+        public static VestObject VestBoth;
 
 
-        private LinkedList<HapticEvent> eventList = new LinkedList<HapticEvent>();
+        private LinkedList<HapticEvent> sym_eventList = new LinkedList<HapticEvent>();
+        private LinkedList<HapticEvent> non_sym_eventList = new LinkedList<HapticEvent>();
         public HapticVestPlugin() {
             // init bhaptics vest;
+            VestLeft = new VestObject("左浮動五顆正面");
+            VestRight = new VestObject("右浮動五顆正面");
+            VestBoth = new VestObject("對稱漸弱2");
+
+            //這個path.tact要放的資料夾
+            Console.WriteLine(this.assembly_location);
+            Console.WriteLine("Initializing...");
+
+            //連這支扣
+            bHapticsManager.Connect("bHapticsLib", "TestApplication", maxRetries: 0);
+            Thread.Sleep(10);
+
+            //確認連上才會繼續初始化
+            Connection = new bHapticsConnection("bHapticsLib2", "AdditionalConnection", maxRetries: 0);
+            Connection.BeginInit();
+            Console.WriteLine(Connection.Status);
         }
-        public void msg_handler(string type, string msg) {
+        public void sym_msg_handler(string type, string msg) {
             // 解析msg
+            Console.WriteLine(type+msg);
             string[] haptic_event_info_lists = msg.Split('|');
             string[] state_info_list = haptic_event_info_lists[4].Split(' ');
             // 做成HapticEvent
@@ -64,13 +91,61 @@ namespace HapticDevicePlugin_HapticVest {
                 msg: msg
             ); 
             // add into eventList
-            lock (eventList) {
-                eventList.AddLast(temp);
+            lock (sym_eventList) {
+                sym_eventList.AddLast(temp);
             }
         }
+        public void non_sym_msg_handler(string type, string msg) {
+            // 解析msg
+            Console.WriteLine(type + msg);
+            string[] haptic_event_info_lists = msg.Split('|');
+            string[] state_info_list = haptic_event_info_lists[4].Split(' ');
+            // 做成HapticEvent
+            HapticEvent temp = new HapticEvent(
+                EventTime: haptic_event_info_lists[0],
+                SourceTypeName: haptic_event_info_lists[1],
+                EventType: haptic_event_info_lists[2],
+                EventName: haptic_event_info_lists[3],
+                Amp: state_info_list[1],
+                Freq: state_info_list[3],
+                Dur: state_info_list[5],
+                EnListTime: DateTime.Now,
+                msg: msg
+            );
+            // add into eventList
+            lock (non_sym_eventList) {
+                non_sym_eventList.AddLast(temp);
+            }
+
+        }
         public void plugin_thread() {
+            HapticEvent nowEvent;
             while (true) {
-                if(eventList.Count > 0) { 
+                if(sym_eventList.Count > 0) {
+                    lock (sym_eventList) {
+                        nowEvent = sym_eventList.First();
+                    }
+                    ScaleOption sclaleoption = new ScaleOption();
+                    sclaleoption.Intensity = nowEvent.get_amplitude;
+                    sclaleoption.Duration = nowEvent.get_duration + 0.2f;
+                    VestBoth.haptic_pattern.Play(sclaleoption);
+                    Console.WriteLine("vibrate both");
+                    lock (sym_eventList) {
+                        sym_eventList.RemoveFirst();
+                    }
+                }
+                if (non_sym_eventList.Count > 0) {
+                    lock (non_sym_eventList) {
+                        nowEvent = non_sym_eventList.First();
+                    }
+                    ScaleOption sclaleoption = new ScaleOption();
+                    sclaleoption.Intensity = nowEvent.get_amplitude/3;
+                    sclaleoption.Duration = nowEvent.get_duration + 0.2f;
+                    VestLeft.haptic_pattern.Play(sclaleoption);
+                    Console.WriteLine("vibrate left");
+                    lock (non_sym_eventList) {
+                        non_sym_eventList.RemoveFirst();
+                    }
 
                 }
             }
@@ -85,7 +160,7 @@ namespace HapticDevicePlugin_HapticVest {
         public HapticPattern haptic_pattern { get; set; }
         public VestObject(string name) {
             this.name = name;
-            this.assembly_location = Path.GetDirectoryName(typeof(VestController).Assembly.Location);
+            this.assembly_location = Path.GetDirectoryName(typeof(VestObject).Assembly.Location);
             this.tactfile_path = Path.Combine(this.assembly_location, this.name + ".tact");
             this.haptic_pattern = HapticPattern.LoadFromFile(this.name, this.tactfile_path);
         }
--- VibrationSignalClassifier/VibrationSignalClassifier/Program.cs
+++ VibrationSignalClassifier/VibrationSignalClassifier/Program.cs
@@ -13,12 +13,14 @@ namespace vibration_signal_classifier {
 
         private static Subscriber VibrationSignalSubscriber = new Subscriber("localhost", 6379);
         private static string VibrationSignalChannelName;
-        private static Thread subThread = new Thread(SubscribeThread);
-
         private static VibrationSignalClassifier vibrationSignalClassifier = new VibrationSignalClassifier();
+
         static void Main(string[] args) {
             VibrationSignalChannelName = args[0];
+            Thread subThread = new Thread(SubscribeThread);
+            Thread classifierThread = new Thread(vibrationSignalClassifier.classifier_thread);
             subThread.Start();
+            classifierThread.Start();
             Console.WriteLine("normal start");
             Console.WriteLine("press enter to stop");
             while (true) {
@@ -26,6 +28,7 @@ namespace vibration_signal_classifier {
                 switch (key.Key) {
                     case ConsoleKey.Enter:
                         subThread.Abort();
+                        classifierThread.Abort();
                         return;
                     default:
                         break;
@@ -73,6 +76,7 @@ namespace vibration_signal_classifier {
             string[] haptic_event_info_lists = msg.Split('|');
             string[] state_info_list = haptic_event_info_lists[4].Split(' ');
             // 做成HapticEvent
+            //Console.WriteLine(msg);
             HapticEvent temp = new HapticEvent(
                 EventTime: haptic_event_info_lists[0],
                 SourceTypeName: haptic_event_info_lists[1],
@@ -111,15 +115,19 @@ namespace vibration_signal_classifier {
                         Console.WriteLine($"SYM  {nowEvent.get_event_daytime.ToString("MM/dd HH:mm:ss.fff", new CultureInfo("en-US")) + "  :"}|{nowEvent.get_source_type_name}|{nowEvent.get_amplitude}|{nowEvent.get_freqeuncy}|{nowEvent.get_duration}|{nowEvent.get_event_name}");
                         // sym_pattern_1(ref nowEvent);
                         // publish symmetrical event
-                        symmetrical_event_publisher.Publish("nonsymmetrical_event", nowEvent.ToString()); // 要publish什麼是隨便填的 之後再補
+                        symmetrical_event_publisher.Publish("symmetrical_event", nowEvent.get_msg);
+                        //Console.WriteLine("sym pub:" + nowEvent.get_msg);
                     }
                     else {
                         Console.WriteLine($"NONE {nowEvent.get_event_daytime.ToString("MM/dd HH:mm:ss.fff", new CultureInfo("en-US")) + "  :"}|{nowEvent.get_source_type_name}|{nowEvent.get_amplitude}|{nowEvent.get_freqeuncy}|{nowEvent.get_duration}|{nowEvent.get_event_name}");
                         // nonsym_pattern_1(ref nowEvent);
                         // publish nonsymmetrical event
-                        nonsymmetrical_event_publisher.Publish("symmetrical_event", nowEvent.ToString()); // 要publish什麼是隨便填的 之後再補
+                        nonsymmetrical_event_publisher.Publish("nonsymmetrical_event", nowEvent.get_msg);
+                        //Console.WriteLine("non pub:" + nowEvent.get_msg);
                     }
-                    
+                    // 05/11 06:43:52.024  :RightController|Output|Vibration|Amp 0.1600 Freq 1.0000 Duration 0.0000
+                    // NONE 05 / 11 06:46:35.000  :| RightController | 0.16 | 1 | 0 | Vibration
+                    // non pub:5 / 11 / 2023 6:46:35 AM | RightController | Output | Vibration | Amp 0.1600 Freq 1.0000 Duration 0.0000
                     lock (eventList) {
                         eventList.RemoveFirst();
                     }
